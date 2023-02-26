6973cecac1c50c97351a40613ed2659277b24e0f
FrothOS <das.shubasis2003@gmail.com>
2023-02-26 19:41:56 +0530

    DSA Lesson 26/02/23

--- BinarySearch.java
+++ BinarySearch.java
@@ -73,8 +73,7 @@ public class BinarySearch {
                     return mid;
                 }
             }
-        }
-        else if (arr[start] < arr[end]) {
+        } else if (arr[start] < arr[end]) {
             while (start <= end) {
                 int mid = start + (end - start) / 2;
                 if (target < arr[mid]) {
@@ -88,15 +87,48 @@ public class BinarySearch {
         }
         return -1;
     }
+
+    // Q. :: Ceiling of an array
+    /* least number in array which is greater than or equal to target value */
+    static int ceil(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        if (arr[start] > arr[end]) {
+            while (start <= end) {
+                int mid = start + (end - start) / 2;
+                if (target > arr[mid]) {
+                    end = mid - 1;
+                } else if (target < arr[mid]) {
+                    start = mid + 1;
+                } else {
+                    return mid;
+                }
+            }
+        } else if (arr[start] < arr[end]) {
+            while (start <= end) {
+                int mid = start + (end - start) / 2;
+                if (target < arr[mid]) {
+                    end = mid - 1;
+                } else if (target > arr[mid]) {
+                    start = mid + 1;
+                } else {
+                    return mid;
+                }
+            }
+        }
+        return start;
+    }
+
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 54, 78, 90 };
-        int[] arr2 = {  90,78,54 , 3, 2, 1 };
+        int[] arr2 = { 90, 78, 54, 3, 2, 1 };
 
         int bin = binSortOA(arr, 54);
-        int bin2 = binSortOA(arr2, 78);
+        int bin2 = binSortOA(arr2, 11);
+        int ceil = ceil(arr, 55);
         if (bin != -1) {
             System.out.println(String.format("array found at position %d", bin));
-            System.out.println(String.format("array found at position %d", bin2));
+            System.out.println(String.format("ceil of the given array %d", arr[ceil]));
         } else {
             System.out.println("array not found");
         }
